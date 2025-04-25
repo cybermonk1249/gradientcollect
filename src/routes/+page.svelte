a3e39b9ad0542f7b5f6a6e0481e16e294f55b98d
cybermonk1249 <cybermonk1924@gmail.com>
2025-04-26 00:50:23 +0200

    added sliding menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
 <script>
     import { base } from "$app/paths";
-    import Hamburger from "$lib/Hamburger.svelte";
+    import { Hamburger } from 'svelte-hamburgers';
+    import { fly } from 'svelte/transition';
+
+    let open = $state(false);
+
 </script>
 
 
 <nav>
     <div class="fullheader">
         <div class="menu">
-            <Hamburger />
+            <Hamburger 
+                bind:open
+                type="emphatic-r"
+                ariaControls="innerheader"            
+            />
         </div>
+        {#if open}
+            <div id="innerheader" transition:fly={{ x: "-100vw", duration: 1000, opacity: 1}}>
+                <ul class="innerlinks">
+                    <li><a class="headerlink" href="{base}/about">about</a></li>
+                    <li><a class="headerlink" href="{base}/about">music</a></li>
+                    <li><a class="headerlink" href="{base}/about">temp</a></li>
+                </ul>
+            </div>
+        {/if}
         <div id="headerlogo">
             <a href="{base}/">
                 <h1>Gradient</h1>
@@ -44,13 +61,17 @@
         font-weight: 100 900;
     }
 
+    :global(body) {
+        margin: 0;
+    }
+    
     * {
         margin: 0;
         padding: 0;
     }
     h1 {
         font-family: Rabegs;
-        font-size: 2.5em;
+        font-size: 1.5em;
     }
 
     nav {
@@ -66,6 +87,7 @@
         grid-template-columns: 4rem 1fr 4rem;
         grid-column-gap: 10px;
         overflow: hidden;
+        height: 100%;
     }
 
     #headerlogo a {
@@ -85,7 +107,41 @@
         border-right: black solid;
         height: 4rem;
         width: 4rem;
+        z-index: 5;
+        background-color: white;
     }
+
+    a, li {
+        list-style: none;
+        text-decoration: none;
+        color: black;
+        font-family: Tektur;
+        font-size: 1.5em;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-content: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    #innerheader {
+        position: absolute;
+        width: calc(100vw - 4rem);
+        height: 4rem;
+        /* left: -100vw; */
+        left: 4rem;
+        background-color: white;
+        /* z-index: 4; */
+        border-right: black solid;
+    }
+
+
+    
 
     
 </style>
