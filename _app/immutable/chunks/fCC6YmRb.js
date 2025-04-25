b9843afda650f66ad27f4371269239ab343c06d8
cybermonk1249 <cybermonk1924@gmail.com>
2025-04-25 15:00:53 +0200

    Updates

--- _app/immutable/chunks/fCC6YmRb.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_14mztau)==null?void 0:s.base)??"/gradientcollect";var t;const e=((t=globalThis.__sveltekit_14mztau)==null?void 0:t.assets)??a;export{e as a,a as b};
