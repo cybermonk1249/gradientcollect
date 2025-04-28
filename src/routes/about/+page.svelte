b8d106c14193f3ad193dcb8208bc745b5f1350dd
cybermonk1249 <cybermonk1924@gmail.com>
2025-04-29 01:13:11 +0200

    added basic about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,18 @@
 <script>
     import { base } from "$app/paths";
+    import Navbar from "$lib/Navbar.svelte"
+    import AudioPlayer from "$lib/AudioPlayer.svelte"
+
 </script>
 
-<a href="{base}/">home</a>
+<Navbar></Navbar>
+
+<p> about about about about blah blah</p>
+
+<AudioPlayer></AudioPlayer>
+
+<style>
+    * {
+        color: white;
+    }
+</style>
